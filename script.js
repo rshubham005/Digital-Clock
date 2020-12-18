0c71c0d51ec87a858d793e4a1922819f46c9fa07
Shubham Rathor <rshubham005@users.noreply.github.com>
2020-12-18 20:07:56 +0530

    Digital clock app

---
+++ script.js
@@ -0,0 +1,41 @@
+var settime= function()
+{
+var ele = document.getElementById("time")
+var currentTime = new Date()
+var hour = currentTime.getHours()
+var min =  currentTime.getMinutes()
+var sec = currentTime.getSeconds()
+var merdian = "PM";
+if(hour>=0 && hour<12)
+{
+    merdian="AM"
+    ele.innerHTML = hour+":"+min+":"+sec+" "+merdian;
+}
+else if(hour>=12 && hour<=23)
+{
+ merdian="PM"
+ if(hour>12)
+ {
+ ele.innerHTML = Number(hour-12)+":"+min+":"+sec+" "+merdian;
+ }
+}
+var img = document.getElementById("img")
+if(hour>=4 && hour<12)
+{
+    img.src="images/goodmorning.jpg"
+}
+else if(hour>=12 && hour<20)
+{
+  img.src ="images/goodafternoon.jpg"   
+}
+else if( hour>=20 || hour<4)
+{
+    img.src="images/goodnight.jpg"
+}
+};
+
+setInterval( settime, 1000);
+function fire()
+{
+    createFirework(53,169,7,3,50,50,19,32,false,true);
+}
